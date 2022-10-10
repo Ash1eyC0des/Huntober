e9038529ba848371118d36317dde93aa5dba46f8
Ashley Christman <dev@ashleychristman.com>
2022-10-10 15:00:02 -0400

    Complete week 2, day 2 challenge

--- BlawsCodingChallenges/array-week.js
+++ BlawsCodingChallenges/array-week.js
@@ -1,3 +1,4 @@
+// Day 1
 function checkEquality(arr1, arr2) {
 	if (arr1.length === arr2.length) {
 		function countArrays(arr) {
@@ -43,7 +44,29 @@ const arr4 = [
 // if we were to use our function to compare all the possible pairs
 // ONLY arr1 vs arr2 would return true
 
-console.log(checkEquality(arr1, arr2)) // true
+// checkEquality(arr1, arr2) // true
 // checkEquality(arr1, arr3) // false
 // checkEquality(arr1, arr4) // false
 // etc.
+
+// Day 2
+function moveLeft(arr, val) {
+	const valIndex = arr.indexOf(val)
+	const leftOfValIndex = valIndex - 1
+	if (valIndex !== 0) {
+		arr.splice(leftOfValIndex, 2, val, arr[leftOfValIndex])
+	}
+	return arr
+}
+
+function moveRight(arr, val) {
+	const valIndex = arr.indexOf(val)
+	const rightOfValIndex = valIndex + 1
+	if (valIndex !== arr.length - 1) {
+		arr.splice(valIndex, 2, arr[rightOfValIndex], val)
+	}
+	return arr
+}
+
+const myArray = ['abc', 'xyz', 1, 2, 'Hey!']
+// console.log(moveRight(myArray, 2))
