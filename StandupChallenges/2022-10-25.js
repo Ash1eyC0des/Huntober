1e0a713fd21ab00ca644a597d9b9845d51445113
Ashley Christman <dev@ashleychristman.com>
2022-10-26 09:18:27 -0400

    Optimize 2022-10-25 challenge

--- StandupChallenges/2022-10-25.js
+++ StandupChallenges/2022-10-25.js
@@ -10,9 +10,11 @@
 // solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]),72)
 
 function solve(arr) {
-	return arr
-		.map((e) => [...new Set(e)])
-		.reduce((prev, curr) => prev * curr.length, 1)
+	return (
+		arr
+			// .map((e) => [...new Set(e)])
+			.reduce((prev, curr) => prev * new Set(curr).size, 1)
+	)
 }
 
 // Tests
